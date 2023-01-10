75b297b3990336bf0fb08ae8d31b73fe3a57f4f1
nhkiiim <nhkiiim@naver.com>
2023-01-10 18:13:55 +0900

    Feat: WeatherApp 레이아웃 잡기

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <View style={styles.weather}>
+        </View>
+      </View>
   );
 }
+
+//별도의 파일로 만들기도 함
+const styles = StyleSheet.create({
+  container: {
+     flex: 1, 
+     backgroundColor: "tomato", 
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 38,
+    fontWeight: "300"
+  },
+  weather: {
+    flex: 3,
+  }
+})
