afad838ecda51700841cf0b2baec5e64a1e391ae
nhkiiim <nhkiiim@naver.com>
2023-01-12 17:33:08 +0900

    Feat: ScrollView 추가

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 export default function App() {
   return (
@@ -8,28 +7,56 @@ export default function App() {
         <View style={styles.city}>
           <Text style={styles.cityName}>Seoul</Text>
         </View>
-        <View style={styles.weather}>
-        </View>
+        <ScrollView horizontal  contentContainerStyle={styles.weather}>
+            <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+            </View>
+           <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+           </View>
+           <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+           </View>
+           <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+           </View>
+        </ScrollView>
       </View>
   );
 }
 
-//별도의 파일로 만들기도 함
 const styles = StyleSheet.create({
   container: {
      flex: 1, 
-     backgroundColor: "tomato", 
+     backgroundColor: "gold",
   },
   city: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   cityName: {
-    fontSize: 38,
-    fontWeight: "300"
+    fontSize: 48,
+    fontWeight: "500",
   },
   weather: {
-    flex: 3,
+
+  },
+  day: {
+    width: 300,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 40,
+    fontSize: 128,
+    fontWeight: "600",
+  },
+  description: {
+    marginTop: -10,
+    fontSize: 40,
   }
 })
